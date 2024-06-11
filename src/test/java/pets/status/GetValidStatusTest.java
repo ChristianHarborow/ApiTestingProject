7e5c771678702e96c66ea73909e204e490f9c242
Duncan <mspam3453@gmail.com>
2024-06-11 14:21:26 +0100

    added status tests

---
+++ src/test/java/pets/status/GetValidStatusTest.java
@@ -0,0 +1,47 @@
+package pets.status;
+
+import config.TestConfig;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class GetValidStatusTest {
+
+    private static Response response;
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = TestConfig.getBaseUri();
+
+
+        response = RestAssured
+                .given()
+                .header("x-api-key", TestConfig.getAPIKey())
+                .queryParam("status", "pending")
+                .when()
+                .get("/pet/findByStatus")
+                .then()
+                .extract()
+                .response();
+    }
+
+    @Test
+    public void testGetPetsByStatus() {
+        MatcherAssert.assertThat(response.getStatusCode(), Matchers.is(200));
+        response.then().body("status", everyItem(equalTo("pending")));
+
+        List<Map<String, Object>> pets = response.jsonPath().getList("$");
+        MatcherAssert.assertThat(pets.size(), greaterThan(0));
+    }
+
+
+}
