3ae1e1e54935f9f95327c77e57b63d52d6d8a18b
christian harborow <christianharborow@hotmail.co.uk>
2024-06-20 15:31:41 +0100

    merges userUtils and utils then refactors request methods

--- src/test/java/user/UserUtils.java
+++ src/test/java/user/UserUtils.java
@@ -2,47 +2,105 @@ package user;
 
 import config.TestConfig;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import pojos.User;
 
 import java.util.Map;
 
 public class UserUtils {
-    private static final String BASE_URI = TestConfig.getBaseUri();
-    private static final String KEY = TestConfig.getAPIKey();
+    private static final String POST_USER_PATH = "/user";
+    private static final String SPECIFIC_USER_PATH = "/user/{username}";
+    private static final String LOGIN_PATH = "/user/login";
+    private static final String LOGOUT_PATH = "/user/logout";
+    private static final String POST_USER_LIST_PATH = "/user/createWithList";
 
-    public Response getUser(String username){
-        return RestAssured
-                .given()
-                .baseUri(BASE_URI)
-                .headers(Map.of(
-                        "api_key",KEY
-                ))
-                .basePath("/user/{username}")
-                .pathParams(Map.of(
-                        "username",username
-                ))
-                .when()
-                .get()
-                .thenReturn();
+    private static RequestSpecBuilder getCommonBuilder(String path) {
+        return new RequestSpecBuilder()
+                .setBaseUri(TestConfig.getBaseUri())
+                .setBasePath(path)
+                .addHeaders(Map.of(
+                        "api_key", TestConfig.getAPIKey(),
+                        "Content-Type", "application/json"
+                ));
     }
 
-    public Response updateUser(String username,User newData){
+    public static RequestSpecification getPostUserRequestSpec(UserBody user) {
+        return getCommonBuilder(POST_USER_PATH)
+                .setBody(user)
+                .build();
+    }
+
+    public static RequestSpecification getPostUserListRequestSpec(UserBody[] users) {
+        return getCommonBuilder(POST_USER_LIST_PATH)
+                .setBody(users)
+                .build();
+    }
+
+    public static RequestSpecification getUpdateUserRequestSpec(String username, User user) {
+        return getCommonBuilder(SPECIFIC_USER_PATH)
+                .addPathParams(Map.of("username", username))
+                .setBody(user)
+                .build();
+    }
+
+    public static RequestSpecification getLoginRequestSpec(String username, String password) {
+        return getCommonBuilder(LOGIN_PATH)
+                .addQueryParam("username", username)
+                .addQueryParam("password", password)
+                .build();
+    }
+
+    public static RequestSpecification getSpecificUserRequestSpec(String username) {
+        return getCommonBuilder(SPECIFIC_USER_PATH)
+                .addPathParams(Map.of("username", username))
+                .build();
+    }
+
+    public static RequestSpecification getLogoutRequestSpec() {
+        return getCommonBuilder(LOGOUT_PATH).build();
+    }
+
+    public static Response postUser(UserBody body) {
         return RestAssured
-                .given()
-                    .baseUri(BASE_URI)
-                    .headers(Map.of(
-                            "api_key",KEY
-                    ))
-                    .basePath("/user/{username}")
-                    .pathParams(Map.of(
-                            "username",username
-                    ))
-                    .contentType(ContentType.JSON)
-                    .body(newData)
-                .when()
-                .put()
-                .thenReturn();
+                .given(getPostUserRequestSpec(body))
+                .post();
+    }
+
+    public static Response postUserList(UserBody[] body) {
+        return RestAssured
+                .given(getPostUserListRequestSpec(body))
+                .post();
+    }
+
+    public static Response getUser(String username) {
+        return RestAssured
+                .given(getSpecificUserRequestSpec(username))
+                .get();
+    }
+
+    public static Response updateUser(String username, User user) {
+        return RestAssured
+                .given(getUpdateUserRequestSpec(username, user))
+                .put();
+    }
+
+    public static Response deleteUser(String username) {
+        return RestAssured
+                .given(getSpecificUserRequestSpec(username))
+                .delete();
+    }
+
+    public static Response loginUser(String username, String password) {
+        return RestAssured
+                .given(getLoginRequestSpec(username, password))
+                .get();
+    }
+
+    public static Response logoutUser() {
+        return RestAssured
+                .given(getLogoutRequestSpec())
+                .get();
     }
 }
